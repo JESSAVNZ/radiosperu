257b5cf7ed6e9d1ee808981432c1c919218a14fc
Joseph Almea <info.dondejoseph@gmail.com>
2022-09-11 17:24:28 -0400

    Update data.js

--- src/data.js
+++ src/data.js
@@ -4,13 +4,13 @@ function chillHop() {
     return [
 
         {
-            name:"Rpp Noticias",
-            artist: "La voz de todo el Perú",
+            name:"Vox FM",
+            artist: "¡Activamos tu mente!",
             cover: "https://i.imgur.com/DSA9eN6.png",
             id: uuidv4(),
-            color: ["#F24933", "#F9B437"],
-            audio: "https://us-b5-p-e-kj2-audio.cdn.mdstrm.com/live-audio-aw/5fab3416b5f9ef165cfab6e9?aid=5faaeb72f92d7b07dfe10181&pid=86qVATUBhSJTpgq5dqfiiOomDhBx4ium&sid=VQkTZAPjdf0AYCdMk6L4DbYmOITGJL6l&uid=l6pavsTygoL8ai2t2wgHXjgKvOdwomhY&es=us-b5-p-e-kj2-audio.cdn.mdstrm.com&ote=1663007987961&ot=V3Qke5jQ7QiVdA78QWuzFw&proto=https&pz=us&cP=128000&awCollectionId=5faaeb72f92d7b07dfe10181&liveId=5fab3416b5f9ef165cfab6e9",
-            active: false,
+            color: ["#1D2731", "#F17E76"],
+            audio: "http://okradio.rtvhd.net:8130/;stream/1",
+            active: true,
         },
         {
             name:"Planeta",
@@ -31,13 +31,13 @@ function chillHop() {
             active: false,
         },
         {
-            name:"Vox FM",
-            artist: "¡Activamos tu mente!",
+            name:"Rpp Noticias",
+            artist: "La voz de todo el Perú",
             cover: "https://i.imgur.com/DSA9eN6.png",
             id: uuidv4(),
-            color: ["#1D2731", "#F17E76"],
-            audio: "http://okradio.rtvhd.net:8130/;stream/1",
-            active: true,
+            color: ["#F24933", "#F9B437"],
+            audio: "https://us-b5-p-e-kj2-audio.cdn.mdstrm.com/live-audio-aw/5fab3416b5f9ef165cfab6e9?aid=5faaeb72f92d7b07dfe10181&pid=86qVATUBhSJTpgq5dqfiiOomDhBx4ium&sid=VQkTZAPjdf0AYCdMk6L4DbYmOITGJL6l&uid=l6pavsTygoL8ai2t2wgHXjgKvOdwomhY&es=us-b5-p-e-kj2-audio.cdn.mdstrm.com&ote=1663007987961&ot=V3Qke5jQ7QiVdA78QWuzFw&proto=https&pz=us&cP=128000&awCollectionId=5faaeb72f92d7b07dfe10181&liveId=5fab3416b5f9ef165cfab6e9",
+            active: false,
         },
     ];
 }
