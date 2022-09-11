3ceca01540026f00faff5d4c0c0bcdf4a2370bc7
Joseph Almea <info.dondejoseph@gmail.com>
2022-09-11 17:22:26 -0400

    Update data.js
    Agregada la radio Vox 105.5 FM

--- src/data.js
+++ src/data.js
@@ -10,7 +10,7 @@ function chillHop() {
             id: uuidv4(),
             color: ["#F24933", "#F9B437"],
             audio: "https://us-b5-p-e-kj2-audio.cdn.mdstrm.com/live-audio-aw/5fab3416b5f9ef165cfab6e9?aid=5faaeb72f92d7b07dfe10181&pid=86qVATUBhSJTpgq5dqfiiOomDhBx4ium&sid=VQkTZAPjdf0AYCdMk6L4DbYmOITGJL6l&uid=l6pavsTygoL8ai2t2wgHXjgKvOdwomhY&es=us-b5-p-e-kj2-audio.cdn.mdstrm.com&ote=1663007987961&ot=V3Qke5jQ7QiVdA78QWuzFw&proto=https&pz=us&cP=128000&awCollectionId=5faaeb72f92d7b07dfe10181&liveId=5fab3416b5f9ef165cfab6e9",
-            active: true,
+            active: false,
         },
         {
             name:"Planeta",
@@ -29,6 +29,15 @@ function chillHop() {
             color: ["#1D2731", "#F17E76"],
             audio: "https://19253.live.streamtheworld.com/CRP_RIT.mp3?csegid=1010&dist=1010&tdsdk=js-%40td&pname=TDSdk&pversion=%40td&banners=none&sbmid=c7e4ba3c-f57d-4a0c-97e0-dd8dfb3a1f91",
             active: false,
+        },
+        {
+            name:"Vox FM",
+            artist: "¡Activamos tu mente!",
+            cover: "https://i.imgur.com/DSA9eN6.png",
+            id: uuidv4(),
+            color: ["#1D2731", "#F17E76"],
+            audio: "http://okradio.rtvhd.net:8130/;stream/1",
+            active: true,
         },
     ];
 }
